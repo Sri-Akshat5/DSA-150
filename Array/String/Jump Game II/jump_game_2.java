8f4c4dd9c6743762e4d3d3b57963dccc5a7a5a62
Akshat Srivastava <akshatsrivastava566@gmail.com>
2025-03-20 18:27:32 +0530

    jump_game_2

--- Array/String/Jump Game II/jump_game_2.java
+++ Array/String/Jump Game II/jump_game_2.java
@@ -1,5 +1,21 @@
 
 
 public class jump_game_2 {
+    public static void main(String[] args){
+        int arr[] = new int[]{1,2,3,4,5,6};
+
+        int lastJump = 0;
+        int far = 0;
+        int jumpCount =0;
+
+        for(int i =0; i<arr.length-1; i++){
+            far = Math.max(far, (i+arr[i]));
+            if(i==lastJump){
+                jumpCount++;
+                lastJump = far;
+            }
+        }
+        System.err.println(jumpCount);
+    }
     
 }
